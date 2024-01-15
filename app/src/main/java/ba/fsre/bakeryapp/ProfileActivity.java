223f5b1f21474f811f5d491e6472c1a2803bc071
Cani Tanushaj <hasan.tanushaj@fsre.sum.ba>
2024-01-15 16:52:11 +0100

    navigacija

--- app/src/main/java/ba/fsre/bakeryapp/ProfileActivity.java
+++ app/src/main/java/ba/fsre/bakeryapp/ProfileActivity.java
@@ -1,11 +1,25 @@
 package ba.fsre.bakeryapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import ba.fsre.bakeryapp.R;
 
 
 
@@ -14,15 +28,58 @@ public class ProfileActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseUser loggedUser;
 
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
         this.auth=FirebaseAuth.getInstance();
-
-
         this.loggedUser=this.auth.getCurrentUser();
 
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//
+//            @SuppressLint("NonConstantResourceId")
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case R.id.categori:
+//                        drawerLayout.closeDrawer(GravityCompat.START);
+//                        Toast.makeText(ProfileActivity.this,"Kategorije",Toast.LENGTH_SHORT).show();
+//
+//                        break;
+//                    case R.id.history:
+//                        drawerLayout.closeDrawer(GravityCompat.START);
+//                        Toast.makeText(ProfileActivity.this,"Povijest",Toast.LENGTH_SHORT).show();
+//                        fragmentR(new HistoryFragment());
+//                        break;
+//
+//
+//                }
+//                return true;
+//            }
+//        });
+//
+//
+//        }
+//    private void fragmentR(Fragment fragment) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.remove(R.id.frameLayout, fragment);
+//        fragmentTransaction.commit();
     }
 }
